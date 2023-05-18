0e051716a5d6b1590e446ff1b3ab6de0f2147c58
Jayoung Hong <gripix324@gmail.com>
2023-05-19 04:41:54 +0900

    [Add] 헤더 위치 잡기

--- src/components/Common/Header/Header.jsx
+++ src/components/Common/Header/Header.jsx
@@ -1,7 +1,24 @@
 import styles from './Header.module.css';
 
 function Header() {
-  return <div className={styles.layout}>헤더</div>;
+  return (
+    <div className={styles.layout}>
+      <p className={styles.logo}>로고 자리</p>
+
+      <div className={styles.nav}>
+        <div className={styles.category}>
+          <p>인생샷</p>
+          <p>커뮤니티</p>
+          <p>유기묘 후원</p>
+        </div>
+
+        <div className={styles.category}>
+          <p>로그인</p>
+          <p>회원가입</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
