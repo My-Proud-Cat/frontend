b12fef75c43b4556fe5ff0c1af62af1690cfb334
Jayoung Hong <gripix324@gmail.com>
2023-05-23 03:29:16 +0900

    [Feat] 페이지네이션 구현

--- src/components/Common/Pagination/Pagination.jsx
+++ src/components/Common/Pagination/Pagination.jsx
@@ -1,14 +1,53 @@
-import { useState } from 'react';
 import styles from './Pagination.module.css';
 
-function Pagination() {
-  const [page, setPage] = useState(1);
+function Pagination({ postsPerPage, totalPosts, paginate, currentPage }) {
+  const pageNumbers = [];
 
-  const handlePageChange = (page) => {
-    setPage(page);
-  };
+  for (let i = 1; i <= Math.ceil(totalPosts, postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
-  return <div className={styles.layout}>페이지네이션</div>;
+  return (
+    <div className={styles.layout}>
+      <button
+        className={styles.paginationButton}
+        aria-label="이전 페이지"
+        onClick={() => {
+          paginate(currentPage - 1);
+        }}
+        disabled={currentPage === 1}
+      >
+        이전
+      </button>
+
+      <ul className={styles.paginate}>
+        {pageNumbers.map((number) => (
+          <li key={number}>
+            <button
+              onClick={() => {
+                paginate(number);
+              }}
+              className={styles.pageNumber}
+              aria-current={currentPage === number ? 'currentPage' : null}
+            >
+              {number}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button
+        className={styles.paginationButton}
+        aria-label="다음 페이지"
+        onClick={() => {
+          paginate(currentPage + 1);
+        }}
+        disabled={currentPage === pageNumbers.length}
+      >
+        다음
+      </button>
+    </div>
+  );
 }
 
 export default Pagination;
--- src/components/PicturePage/PictureList/PictureList.jsx
+++ src/components/PicturePage/PictureList/PictureList.jsx
@@ -11,10 +11,23 @@ function PictureList() {
   const pictureListData = useRecoilValue(getPictureList);
 
   const [posts, setPosts] = useState([...pictureListData]);
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
+  const [postsPerPage, setPostsPerPage] = useState(3); // 한 페이지에 보일 게시글 수
+
+  const indexOfLast = currentPage * postsPerPage;
+  const indexOfFirst = indexOfLast - postsPerPage;
+
+  const currentPosts = (posts) => {
+    let currentPosts = 0;
+    currentPosts = posts.slice(indexOfFirst, indexOfLast);
+
+    return currentPosts;
+  };
 
   return (
     <>
-      <Banner />
+      <Banner className={styles.banner} />
+
       <div className={styles.layout}>
         <div className={styles.position}>
           <div>
@@ -27,12 +40,17 @@ function PictureList() {
         </div>
 
         <div className={styles.post}>
-          {posts.map((item) => {
+          {currentPosts(posts).map((item) => {
             return <Article key={item?.id} item={item} />;
           })}
         </div>
 
-        <Pagination />
+        <Pagination
+          postsPerPage={postsPerPage}
+          totalPosts={posts.length}
+          paginate={setCurrentPage}
+          currentPage={currentPage}
+        />
       </div>
     </>
   );
