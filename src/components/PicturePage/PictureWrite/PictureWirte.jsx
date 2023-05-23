9e7aeeccf78d6a8127829dc5fc3f094708d7b116
Jayoung Hong <gripix324@gmail.com>
2023-05-24 03:38:23 +0900

    [Add] 인생샷 글쓰기 디자인 구현

--- src/components/PicturePage/PictureWrite/PictureWirte.jsx
+++ src/components/PicturePage/PictureWrite/PictureWirte.jsx
@@ -1,12 +1,15 @@
 import styles from './PictureWirte.module.css';
 import Banner from 'components/Common/Banner/Banner';
+import FormInput from 'components/Common/FormInput/FormInput';
 
 function PictureWirte() {
   return (
     <>
       <Banner />
 
-      <div className={styles.layout}></div>
+      <div className={styles.layout}>
+        <FormInput />
+      </div>
     </>
   );
 }
