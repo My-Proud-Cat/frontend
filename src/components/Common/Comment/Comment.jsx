72dc1cf820d433577dfb88d7cf0c7ab3dda65288
Jayoung Hong <gripix324@gmail.com>
2023-06-01 05:41:25 +0900

    [Add] 컴포넌트 재구성

--- src/components/Common/Comment/Comment.jsx
+++ src/components/Common/Comment/Comment.jsx
@@ -1,59 +1,20 @@
 import styles from './Comment.module.css';
-import SubmitButton from 'components/Common/SubmitButton/SubmitButton';
 import AnswerEditor from 'components/Common/AnswerEditor/AnswerEditor';
-import axios from 'axios';
 import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { getPictureComment } from '@store/getPictureCommentData';
-
-/* 텍스트 지우는 함수 */
-function clearText() {
-  document.getElementById('comment').value = '';
-}
+import WriteInput from 'components/Common/WriteInput/WriteInput';
 
 function Comment() {
   const pictureCommentData = useRecoilValue(getPictureComment);
   const [comment, setComment] = useState([...pictureCommentData]);
-
-  const commentField = document.getElementById('comment');
-
-  async function onClickButton() {
-    await axios
-      .post('http://localhost:3001/comment', {
-        user: {
-          nickname: '임시 닉네임',
-          user_id: '임시 아이디',
-        },
-        comment: commentField.value,
-        created_at: new Date().getTime(),
-      })
-      .then((res) => {
-        console.log(res);
-        clearText();
-        window.location.reload();
-      });
-  }
 
   return (
     <>
       <div className={styles.layout}>
         <p className={styles.label}>댓글</p>
 
-        <div>
-          <textarea
-            id="comment"
-            type="text"
-            placeholder="댓글을 입력해주세요"
-            className={styles.form}
-          />
-
-          <SubmitButton
-            comment="true"
-            onClick={() => {
-              onClickButton();
-            }}
-          />
-        </div>
+        <WriteInput comment="true" />
       </div>
 
       {comment.map((item) => {
--- src/components/Common/WriteInput/WriteInput.jsx
+++ src/components/Common/WriteInput/WriteInput.jsx
@@ -1,27 +1,74 @@
 import { string } from 'prop-types';
 import styles from './WriteInput.module.css';
+import axios from 'axios';
+import SubmitButton from 'components/Common/SubmitButton/SubmitButton';
 
-function WriteInput() {
+/* 텍스트 지우는 함수 */
+function clearText() {
+  document.getElementById('comment').value = '';
+}
+
+function WriteInput({ comment }) {
+  const commentField = document.getElementById('comment');
+
+  async function onClickButton() {
+    await axios
+      .post('http://localhost:3001/comment', {
+        user: {
+          nickname: '임시 닉네임',
+          user_id: '임시 아이디',
+        },
+        comment: commentField.value,
+        created_at: new Date().getTime(),
+      })
+      .then((res) => {
+        console.log(res);
+        clearText();
+        window.location.reload();
+      });
+  }
+
   return (
     <>
-      <div>
-        <p>글 작성</p>
+      {comment ? (
+        <div>
+          <textarea
+            id="comment"
+            type="text"
+            placeholder="댓글을 입력해주세요"
+            className={styles.form}
+          />
 
-        <div className={styles.layout}>
-          <div className={styles.input}>
-            <input className={styles.title} placeholder="제목을 입력해주세요" />
-            <textarea
-              className={styles.content}
-              placeholder="내용을 입력해주세요"
-            />
-          </div>
+          <SubmitButton
+            comment="true"
+            onClick={() => {
+              onClickButton();
+            }}
+          />
+        </div>
+      ) : (
+        <div>
+          <p>글 작성</p>
 
-          <div className={styles.fileUpload}>
-            <label htmlFor="file">사진 추가하기</label>
-            <input id="file" type="file" />
+          <div className={styles.layout}>
+            <div className={styles.input}>
+              <input
+                className={styles.title}
+                placeholder="제목을 입력해주세요"
+              />
+              <textarea
+                className={styles.content}
+                placeholder="내용을 입력해주세요"
+              />
+            </div>
+
+            <div className={styles.fileUpload}>
+              <label htmlFor="file">사진 추가하기</label>
+              <input id="file" type="file" />
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
