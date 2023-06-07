8e9e4cf7f6b1108b049e02e78b63a5a63842b08c
Jayoung Hong <gripix324@gmail.com>
2023-06-08 02:05:18 +0900

    [Add] 닉네임 불러올 때 오류나던 현상 해결 - 옵셔널 체이닝 사용

--- src/components/PicturePage/PictureDetail/PictureDetail.jsx
+++ src/components/PicturePage/PictureDetail/PictureDetail.jsx
@@ -1,22 +1,22 @@
 import styles from './PictureDetail.module.css';
 import Banner from 'components/Common/Banner/Banner';
 import Comment from 'components/Common/Comment/Comment';
-import { useRecoilValue, useRecoilValueLoadable } from 'recoil';
-import { getPictureList, getPicture } from '@store/getPictureData';
+import { useRecoilValueLoadable } from 'recoil';
+import { getPicture } from '@store/getPictureData';
 import { useNavigate, useParams } from 'react-router-dom';
 
 function PictureDetail() {
   const { id } = useParams();
 
   const pictureData = useRecoilValueLoadable(getPicture(id));
-  let item = [pictureData].find((item) => id);
+  let item = [pictureData].find(() => id);
 
   const { title, content, img, user, like, view } = item.contents;
 
-  console.log(item.contents);
+  console.log(user?.nickname);
 
   if (item === 'hasError') {
-    return <div>Error : {item.contents.message}</div>;
+    return <div>Error : {console.log(item.error)}</div>;
   }
 
   if (item === 'loading') {
@@ -32,7 +32,7 @@ function PictureDetail() {
           <p className={styles.title}>{title}</p>
 
           <div className={styles.info}>
-            <p className={styles.nickname}>{user.nickname}</p>
+            <p className={styles.nickname}>{user?.nickname}</p>
             <p>2023.05.20</p>
           </div>
         </div>
