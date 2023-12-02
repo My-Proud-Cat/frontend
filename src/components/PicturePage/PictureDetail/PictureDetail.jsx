9a862d2b522c604a51fb6a34668216171bc9cb10
HJY0 <gripix324@gmail.com>
2023-12-02 16:05:45 +0900

    [Feat] PostUD 내용을 Detail쪽으로 옮기고 UpdateMode 페이지 추가

--- src/components/PicturePage/PictureDetail/PictureDetail.jsx
+++ src/components/PicturePage/PictureDetail/PictureDetail.jsx
@@ -2,19 +2,23 @@ import styles from './PictureDetail.module.css';
 import Banner from 'components/Common/Banner/Banner';
 import Comment from 'components/Common/Comment/Comment';
 import PostUD from 'components/Common/PostUD/PostUD';
+import SubmitButton from 'components/Common/SubmitButton/SubmitButton';
+import UpdatePage from 'components/Common/UpdatePage/UpdatePage';
 import axios from 'axios';
 import { ReactComponent as ThumbsBefore } from 'assets/thumbsBefore.svg';
 import { ReactComponent as ThumbsAfter } from 'assets/thumbsAfter.svg';
 import { useRecoilValueLoadable } from 'recoil';
 import { getPicture } from '@store/getPictureData';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { axiosInstance } from 'custom/authToken';
 
 function PictureDetail() {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const [likeState, setLikeState] = useState('');
+  const [updateMode, setUpdateMode] = useState(false);
 
   const pictureData = useRecoilValueLoadable(getPicture(id));
   let item = [pictureData].find(() => id);
@@ -65,43 +69,116 @@ function PictureDetail() {
       });
   };
 
+  /* ---------------------------------- 수정/삭제 --------------------------------- */
+
+  const onClickUpdateButton = async () => {
+    setUpdateMode(true);
+  };
+
+  const onCliclCancelButton = () => {
+    setUpdateMode(false);
+  };
+
+  const onSubmitUpdateButton = async () => {
+    await axiosInstance
+      .put(`http://localhost:8080/picture/${id}`, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      })
+      .then(() => {
+        console.log('성공');
+      });
+  };
+
+  const onClickDeleteButton = async () => {
+    const ok = window.confirm('삭제 하시겠습니까?');
+
+    if (ok) {
+      await axiosInstance
+        .delete(`http://localhost:8080/picture/${id}`)
+        .then(() => {
+          navigate('/');
+          location.reload();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
   return (
     <>
       <Banner />
 
-      <div className={styles.layout}>
-        <div className={styles.position}>
-          <p className={styles.title}>{title}</p>
+      {updateMode === true ? (
+        <div className={styles.layout}>
+          <UpdatePage />
 
-          <div className={styles.info}>
-            <p className={styles.nickname}>{nickname}</p>
-            <p className={styles.date}>{createdAt}</p>
-            <PostUD />
+          <div className={styles.button}>
+            <SubmitButton category="수정" onClick={onSubmitUpdateButton} />
+            <SubmitButton
+              category="취소"
+              onClick={onCliclCancelButton}
+              type="button"
+            />
           </div>
         </div>
+      ) : (
+        <div className={styles.layout}>
+          <div className={styles.position}>
+            <p className={styles.title}>{title}</p>
 
-        <p className={styles.content}>{describe}</p>
+            <div className={styles.info}>
+              <p className={styles.nickname}>{nickname}</p>
+              <p className={styles.date}>{createdAt}</p>
+              {/* <PostUD /> */}
 
-        <div className={styles.main}>
-          <img src={imageUrl} alt="이미지" className={styles.picture} />
+              <div className={styles.layout2}>
+                <div>
+                  <button
+                    className={styles.update}
+                    onClick={() => {
+                      onClickUpdateButton();
+                    }}
+                  >
+                    수정
+                  </button>
 
-          <button
-            className={likeState === '' ? styles.like_no : styles.like}
-            onClick={() => clickLike()}
-          >
-            <ThumbsBefore
-              className={likeState === '' ? styles.visible : styles.invisible}
+                  <button
+                    className={styles.delete}
+                    onClick={() => {
+                      onClickDeleteButton();
+                    }}
+                  >
+                    삭제
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <p className={styles.content}>{describe}</p>
+
+          <div className={styles.main}>
+            <img src={imageUrl} alt="이미지" className={styles.picture} />
+
+            <button
+              className={likeState === '' ? styles.like_no : styles.like}
+              onClick={() => clickLike()}
             >
-              추천
-            </ThumbsBefore>
-            <ThumbsAfter
-              className={likeState === '' ? styles.invisible : styles.visible}
-            />
-          </button>
-        </div>
+              <ThumbsBefore
+                className={likeState === '' ? styles.visible : styles.invisible}
+              >
+                추천
+              </ThumbsBefore>
+              <ThumbsAfter
+                className={likeState === '' ? styles.invisible : styles.visible}
+              />
+            </button>
+          </div>
 
-        <Comment />
-      </div>
+          <Comment />
+        </div>
+      )}
     </>
   );
 }
