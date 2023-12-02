553bab00473df42667fa5e24c48a95dc7cc4c1a7
HJY0 <gripix324@gmail.com>
2023-12-02 16:57:55 +0900

    [Improve] 기능 부분 Custom Hook 으로 뺌

--- src/components/Common/WriteInput/WriteInput.jsx
+++ src/components/Common/WriteInput/WriteInput.jsx
@@ -1,93 +1,24 @@
 import { string } from 'prop-types';
 import styles from './WriteInput.module.css';
-import axios from 'axios';
 import SubmitButton from 'components/Common/SubmitButton/SubmitButton';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { titleState } from '@store/getPictureTitleData';
-import { contentState } from '@store/getPictureContentData';
-import { useParams, useNavigate } from 'react-router-dom';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { axiosInstance } from 'custom/authToken';
-
-/* 텍스트 지우는 함수 */
-function clearText() {
-  document.getElementById('comment').value = '';
-}
+import useInput from 'custom/useInput';
 
 function WriteInput({ comment }) {
-  const { id } = useParams();
-  const navigate = useNavigate();
-
-  const titleField = useRecoilValue(titleState);
-  const contentField = useRecoilValue(contentState);
-
-  const [title, setTitle] = useRecoilState(titleState);
-  const [content, setContent] = useRecoilState(contentState);
-
-  /* ----------------------------------- 파일 ----------------------------------- */
-
-  const fileInputRef = useRef(null);
-
-  const [image, setImage] = useState();
-  const [imgPreview, setImgPreview] = useState('');
-
-  const onClickFileInput = () => {
-    fileInputRef.current.click(); // 버튼을 클릭하면 input을 참조해서 input이 클릭되게 함 (원래는 눌러도 아무 반응 x - input이 아니라 버튼이 눌린 걸로 판정되기때문)
-  };
-
-  const onChangeFile = async (e) => {
-    if (e.target.files[0] !== null) {
-      setImage(e.target.files[0]);
-
-      setImgPreview(URL.createObjectURL(e.target.files[0]));
-    }
-  };
-
-  /* ------------------------------------ 글 ----------------------------------- */
-
-  const formData = new FormData();
-
-  formData.append('image', image);
-
-  const postData = {
-    title: titleField,
-    describe: contentField,
-  };
-
-  const json = JSON.stringify(postData);
-  const blob = new Blob([json], {
-    type: 'application/json',
-  });
-
-  formData.append('request', blob);
-
-  async function onClickPostButton(e) {
-    e.preventDefault();
-
-    await axiosInstance
-      .post('http://localhost:8080/picture', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      })
-      .then(() => {
-        navigate('/');
-        location.reload();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    for (let key of formData.entries()) {
-      console.log(key);
-    }
-  }
-
-  const onChangeTitle = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const onChangeContent = (e) => {
-    setContent(e.target.value);
-  };
+  const {
+    id,
+    clearText,
+    onClickFileInput,
+    onChangeFile,
+    onClickPostButton,
+    onChangeTitle,
+    onChangeContent,
+    title,
+    content,
+    imgPreview,
+    fileInputRef,
+  } = useInput();
 
   /* ----------------------------------- 댓글 ----------------------------------- */
 
