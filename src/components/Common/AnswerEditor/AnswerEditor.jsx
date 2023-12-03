2cf612f1b9c3439fc4f453838ed8d7c92bae67db
Jayoung Hong <gripix324@gmail.com>
2023-12-03 13:47:26 +0900

    [Add] 로그인 사용자와 댓글 작성자가 같을 경우에만 삭제가 가능하게 설정

--- src/components/Common/AnswerEditor/AnswerEditor.jsx
+++ src/components/Common/AnswerEditor/AnswerEditor.jsx
@@ -3,13 +3,19 @@ import { getPictureComment } from '@store/getPictureCommentData';
 import { useParams } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { axiosInstance } from 'custom/authToken';
+import { useState, useEffect } from 'react';
 
 function AnswerEditor({ item }) {
   const { id } = useParams();
 
   const commentData = useRecoilState(getPictureComment(id));
 
-  const clickDeleteButton = async (e) => {
+  const { commentWriter, createdAt, content, email } = item;
+
+  const storage = localStorage.getItem('accessToken');
+  const [auth, setAuth] = useState(false);
+
+  const clickDeleteButton = async () => {
     const ok = window.confirm('삭제 하시겠습니까?');
 
     if (ok) {
@@ -24,26 +30,44 @@ function AnswerEditor({ item }) {
     }
   };
 
+  useEffect(() => {
+    if (storage) {
+      axiosInstance
+        .get('http://localhost:8080/auth/user-detail')
+        .then((response) => {
+          if (response.data.email === email) {
+            setAuth(true);
+          }
+        });
+    } else {
+      setAuth(false);
+    }
+  }, []);
+
   return (
     <>
       <div className={styles.comment}>
         <div className={styles.info}>
-          <p className={styles.name}>{item.commentWriter}</p>
+          <p className={styles.name}>{commentWriter}</p>
 
           <div className={styles.position}>
-            <p className={styles.date}>{item.createdAt}</p>
-            <button
-              className={styles.delete}
-              onClick={() => {
-                clickDeleteButton();
-              }}
-            >
-              삭제
-            </button>
+            <p className={styles.date}>{createdAt}</p>
+            {auth === true ? (
+              <button
+                className={styles.delete}
+                onClick={() => {
+                  clickDeleteButton();
+                }}
+              >
+                삭제
+              </button>
+            ) : (
+              <></>
+            )}
           </div>
         </div>
 
-        <p className={styles.answer}>{item.content}</p>
+        <p className={styles.answer}>{content}</p>
       </div>
     </>
   );
