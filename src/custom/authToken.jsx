b85cbb67c8b027498f098a613b1294ff34894a00
Jayoung Hong <gripix324@gmail.com>
2024-02-04 00:38:35 +0900

    [Add] Token 관련 오류 부분 삭제

--- src/custom/authToken.jsx
+++ src/custom/authToken.jsx
@@ -12,58 +12,17 @@ const postRefreshToken = async () => {
   return response;
 };
 
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const {
-      config,
-      response: { status },
-    } = error;
-
-    console.log(error);
-
-    const originalRequest = config;
-
-    // localStorage.removeItem('accessToken');
-    // location.reload();
-
-    if (status === 401) {
-      console.log('토큰 만료');
-
-      /* try {
-        const { data } = await axios.post(
-          'http://localhost:8080/auth/reissue',
-          {
-            accessToken: localStorage.getItem('accessToken'),
-            refreshToken: localStorage.getItem('refreshToken'),
-          },
-        );
-
-        const newAccessToken = data.data.accessToken;
-        const newRefreshToken = data.data.refreshToken;
-
-        originalRequest.headers['Authorization'] = 'Bearer' + newAccessToken;
-
-        localStorage.setItem('accessToken', newAccessToken);
-        localStorage.setItem('refreshToken', newRefreshToken);
-
-        return await axios(originalRequest);
-      } catch (err) {
-        new Error(err);
-      } */
-    }
-
-    return Promise.reject(error);
-  },
-);
+axiosInstance.interceptors.response.use((response) => {
+  return response;
+});
 
 axiosInstance.interceptors.request.use(
   (request) => {
     request.headers['Authorization'] = `Bearer ${localStorage.getItem(
       'accessToken',
     )}`;
+
+    console.log(request);
     return request;
   },
   (error) => {
