638ec749b325c9fc9e062e3af5ac0e8ae72752aa
Jayoung Hong <gripix324@gmail.com>
2024-02-17 22:02:03 +0900

    [Design] 디자인 디테일 추가

--- src/components/Mypage/MyPageUpdate/MyPageUpdate.jsx
+++ src/components/Mypage/MyPageUpdate/MyPageUpdate.jsx
@@ -10,72 +10,101 @@ const MyPageUpdate = () => {
       <div className={styles.layout2}>
         <p className={styles.title}>프로필 수정</p>
 
-        <div className="">
-          <form action="">
-            <table className={styles.table}>
-              <tbody>
-                <tr className={styles.line}>
-                  <th>프로필 사진</th>
+        <form action="">
+          <table className={styles.table}>
+            <tbody>
+              <tr className={styles.line}>
+                <th>프로필 사진</th>
 
-                  <td>
-                    <div className="">
-                      <img src="" alt="" className={styles.profile_img} />
+                <td>
+                  <div className="">
+                    <img src="" alt="" className={styles.profile_img} />
 
-                      <div className={styles.profile_button}>
-                        <button name="change">사진변경</button>
-                        <button name="delete">삭제</button>
-                      </div>
+                    <div className={styles.profile_button}>
+                      <button name="change">사진변경</button>
+                      <button name="delete">삭제</button>
                     </div>
-                  </td>
-                </tr>
-                <tr>
-                  <th>정보 수정</th>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <th>정보 수정</th>
 
-                  <td className={styles.info_layout}>
-                    <div className={styles.info}>
-                      <div className={styles.nickname}>
-                        <p>닉네임</p>
-                        <input type="text" name="nickname" />
-                      </div>
-
-                      <div className={styles.cat_check}>
-                        <p>고양이</p>
-                        <input type="radio" name="true" />
-                        있어요
-                        <input type="radio" name="false" />
-                        없어요
-                      </div>
+                <td className={styles.info_layout}>
+                  <div className={styles.info}>
+                    <div className={styles.nickname}>
+                      <p>닉네임</p>
+                      <input type="text" name="nickname" />
                     </div>
 
-                    <div className={styles.info_detail}>
-                      <div className={styles.name}>
-                        <p>고양이 이름</p>
-                        <input type="text" name="name" />
-                      </div>
-
-                      <div className={styles.name}>
-                        <p>고양이 종</p>
-                        <input type="text" name="name" />
-                      </div>
-
-                      <div className={styles.name}>
-                        <p>고양이 성별</p>
-                        <input type="text" name="name" />
-                      </div>
-
-                      <div className={styles.name}>
-                        <p>고양이 이름</p>
-                        <input type="text" name="name" />
-                      </div>
+                    <div className={styles.cat_check}>
+                      <p>고양이</p>
+                      <input type="radio" name="cat" value="true" />
+                      있어요
+                      <input type="radio" name="cat" value="false" />
+                      없어요
                     </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+                  </div>
 
-            <SubmitButton category="수정" />
-          </form>
-        </div>
+                  <div className={styles.info_detail}>
+                    <div className={styles.cat_info}>
+                      <p>고양이 이름</p>
+                      <input type="text" name="name" />
+                    </div>
+
+                    <div className={styles.cat_info}>
+                      <p>고양이 종</p>
+                      <input type="text" name="kind" />
+                    </div>
+
+                    <div className={styles.cat_info}>
+                      <p>고양이 성별</p>
+                      <input type="radio" name="gender" value="male" />
+                      남아
+                      <input type="radio" name="gender" value="female" />
+                      여아
+                    </div>
+
+                    <div className={styles.cat_info}>
+                      <p>고양이 나이</p>
+                      <select name="age">
+                        <option value="unknown">모름</option>
+                        <option value="0">0</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                        <option value="13">13</option>
+                        <option value="14">14</option>
+                        <option value="15">15</option>
+                        <option value="16">16</option>
+                        <option value="17">17</option>
+                        <option value="18">18</option>
+                        <option value="19">19</option>
+                        <option value="20">20</option>
+                        <option value="21">21</option>
+                        <option value="22">22</option>
+                        <option value="23">23</option>
+                        <option value="24">24</option>
+                        <option value="25">25</option>
+                      </select>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <SubmitButton category="수정" />
+        </form>
       </div>
     </div>
   );
