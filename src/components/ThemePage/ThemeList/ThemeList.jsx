18939b38ec0e9a2ac6782b671e5f5b4a7cb2b003
Jayoung Hong <gripix324@gmail.com>
2024-02-21 02:51:22 +0900

    [Add] 테마샷 리스트페이지 기본 프레임 추가

--- src/components/ThemePage/ThemeList/ThemeList.jsx
+++ src/components/ThemePage/ThemeList/ThemeList.jsx
@@ -1,7 +1,50 @@
 import styles from './ThemeList.module.css';
+import { useNavigate } from 'react-router-dom';
+import Banner from 'components/Common/Banner/Banner';
+import Article from 'components/Common/Article/Article';
+import Sort from 'components/Common/Sort/Sort';
+import Pagination from 'components/Common/Pagination/Pagination';
 
 const ThemeList = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const storage = localStorage.getItem('accessToken');
+
+  const onCLickWriteButton = () => {
+    if (storage) {
+      navigate('/write');
+    } else {
+      alert('로그인 후 이용 가능합니다');
+    }
+  };
+
+  return (
+    <>
+      <Banner />
+
+      <div className={styles.layout}>
+        <div className={styles.position}>
+          <div>
+            <button
+              className={styles.writeButton}
+              onClick={() => {
+                onCLickWriteButton();
+              }}
+            >
+              글쓰기
+            </button>
+          </div>
+
+          <p className={styles.line}></p>
+
+          <Sort /* onClick={() => onClickSort(event)} */ />
+        </div>
+
+        <div className={styles.post}>{/* <Article /> */}</div>
+
+        <Pagination />
+      </div>
+    </>
+  );
 };
 
 export default ThemeList;
