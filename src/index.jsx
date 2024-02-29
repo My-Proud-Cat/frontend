40c7a6959c448865b315e9174e21cd5f0efc99d6
HJY0 <gripix324@gmail.com>
2024-02-29 17:11:08 +0900

    [Add] 상위 경로에 CookiesProvider 추가

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { CookiesProvider } from 'react-cookie'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CookiesProvider>
     <BrowserRouter>
       <RecoilRoot>
         <App />
       </RecoilRoot>
     </BrowserRouter>
+    </CookiesProvider>
   </React.StrictMode>,
 );
